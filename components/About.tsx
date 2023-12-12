1758825a8ad23185e957ab4e28dfa1989fcb3eb3
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2023-12-13 03:53:27 +0400

    Edits to Hero section + Started with About Section

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className='flex flex-col md:flex-row w-full h-screen bg-gradient-to-l from-slate-950  via-cyan-950 to-gray-950'>
+      <div className='flex flex-col items-center justify-start w-full pt-6 mx-auto'>
+        <h1 className='text-6xl text-neutral-500'>About Me</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero,
+          ducimus corrupti. Asperiores corporis, libero vitae consectetur error
+          fugiat accusamus quod dignissimos, aspernatur id consequuntur porro
+          rem maxime, reprehenderit earum doloremque! Explicabo, atque corporis.
+          Officiis fuga mollitia eveniet fugit rem excepturi sed in molestias ut
+          repellat blanditiis eaque tempora sit eius, harum dolorem distinctio
+          voluptate pariatur corporis corrupti expedita voluptas est?
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import React from 'react';
+import heroImage from '@/public/hero-image.png'
 import { Typewriter, Cursor } from 'react-simple-typewriter';
 
 const Hero = () => {
@@ -12,28 +13,26 @@ const Hero = () => {
   });
 
   return (
-    <section className='flex flex-col md:flex-row w-full h-screen bg-slate-950'>
-      <div className='flex flex-row items-center justify-center w-full md:pl-40 pl-20'>
-        <div className='h-full w-full flex flex-col justify-start m-auto text-start mt-20 md:mt-40'>
+    <section className='flex flex-col md:flex-row w-full h-screen bg-gradient-to-r from-slate-950  via-cyan-950 to-gray-950'>
+      <div className='flex flex-row items-center justify-center w-full md:pl-40 pl-20 pt-20 md:pt-0'>
+        <div className='h-full w-full flex flex-col justify-center m-auto text-start'>
           <h1 className='text-gray-200 text-3xl'>Welcome</h1>
           <div className='flex flex-col w-auto h-auto'>
             <h3 className='text-red-500 text-5xl font-black'>I&apos;m Name</h3>
             <p className='text-pink-300'>Web Developer</p>
-            <p className='text-emerald-300 text-3xl opacity-25 font-black mt-10'>
+            <p className='text-emerald-300 text-3xl opacity-25 font-black pt-8'>
               {text}
               <span>
-                <Cursor cursorStyle='|' cursorColor='red' />
+                <Cursor cursorStyle='|' cursorColor='yellow' />
               </span>
             </p>
           </div>
         </div>
       </div>
-      <div className='w-full h-full flex justify-center items-center mt-5'>
+      <div className='justify-center items-center pt-5 m-auto mx-20 md:mx-10 md:pr-20'>
         <Image
-          src='/hero-image.png'
+          src={heroImage}
           alt='laptop web design'
-          width={450}
-          height={450}
         />
       </div>
     </section>
