54bf98c45a4340ff2ab206ebd6239d9b1c2643a2
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2023-12-13 04:10:37 +0400

    Smooth-Scroll in layout.tsx and worked on About section

--- components/About.tsx
+++ components/About.tsx
@@ -1,19 +1,52 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const About = () => {
   return (
     <section className='flex flex-col md:flex-row w-full h-screen bg-gradient-to-l from-slate-950  via-cyan-950 to-gray-950'>
-      <div className='flex flex-col items-center justify-start w-full pt-6 mx-auto'>
+      <div className='flex flex-col items-center justify-start w-full pt-6 mx-auto px-20'>
         <h1 className='text-6xl text-neutral-500'>About Me</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero,
-          ducimus corrupti. Asperiores corporis, libero vitae consectetur error
-          fugiat accusamus quod dignissimos, aspernatur id consequuntur porro
-          rem maxime, reprehenderit earum doloremque! Explicabo, atque corporis.
-          Officiis fuga mollitia eveniet fugit rem excepturi sed in molestias ut
-          repellat blanditiis eaque tempora sit eius, harum dolorem distinctio
-          voluptate pariatur corporis corrupti expedita voluptas est?
-        </p>
+
+        {/* Card */}
+        <Link
+          href='#'
+          className='relative block overflow-hidden rounded-lg border bg-slate-700  border-gray-100 p-4 mt-10 sm:p-6 lg:p-8'
+        >
+          <span className='absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'></span>
+
+          <div className='sm:flex sm:justify-between sm:gap-4'>
+            <div>
+              <h3 className='text-lg font-bold text-gray-900 sm:text-xl'>
+                Web Developer
+              </h3>
+
+              <p className='mt-1 text-xs font-medium text-gray-600'>
+                By John Doe
+              </p>
+            </div>
+
+            <div className='hidden sm:block sm:shrink-0'>
+              <Image
+                alt=''
+                src=''
+                className='h-16 w-16 rounded-lg object-cover shadow-sm'
+                width={64}
+                height={64}
+              />
+            </div>
+          </div>
+
+          <div className='mt-4'>
+            <p className='max-w-[40ch] text-sm text-gray-500'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit
+              illum provident a, ipsa maiores deleniti consectetur nobis et
+              eaque.
+            </p>
+          </div>
+
+          
+        </Link>
       </div>
     </section>
   );
