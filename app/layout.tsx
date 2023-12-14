aa2bc0e6803587bfbaa95a5289f53abd368ca42e
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2023-12-15 02:24:38 +0400

    removed themeColor metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: "Adnan's Portfolio",
   description: 'Personal Portfolio site made using Next.JS',
-  themeColor: 'rgb(30,140,140)'
 };
 
 export default function RootLayout({
